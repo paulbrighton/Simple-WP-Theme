3d6a9013a438f6f9df6f80ee67a1d78684a12b3a
Paul Brighton <brighton.paul@yahoo.com>
2020-03-09 12:07:42 +0000

    Added posts to the home page and along with some simple styling

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-  <meta charset="<?php bloginfo('charset'); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?php bloginfo('name'); ?></title>
-  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-  <?php wp_head(); ?>
-</head>
-<body>
-  
-</body>
-</html>
+  <?php get_header(); ?>
+
+  <div class="container container-flex">
+    <div class="main">
+      <?php if (have_posts()) : ?>
+        <?php while (have_posts()) : the_post() ?>
+          <h3><?php the_title(); ?></h3>
+          <div class="meta">
+            Created by <?php the_author(); ?> on <?php the_time('F j, Y g:i a'); ?>
+          </div>
+          <?php the_content(); ?>
+        <?php endwhile; ?>
+      <?php else : ?>
+        <?php echo wpautop('Sorry, no posts were found.') ?>
+      <?php endif; ?>
+    </div>
+  </div>
+
+  <?php get_footer(); ?>
