46262ccadbcad4c6be902d5d8b870078715f85d9
Paul Brighton <brighton.paul@yahoo.com>
2020-03-09 10:53:20 +0000

    Added wp_head and removed styling

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php bloginfo('name'); ?></title>
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+  <?php wp_head(); ?>
 </head>
 <body>
   
