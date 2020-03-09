ccea0e7ad75393beabd1a9c3864fdb8ad70ff759
Paul Brighton <brighton.paul@yahoo.com>
2020-03-09 10:50:16 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php bloginfo('name'); ?></title>
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+<body>
+  
+</body>
+</html>
